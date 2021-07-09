81f2023c6988e0e2d7b35cb2bbaaa47d10640f4a
BOGUEH <85343204+BOGUEH@users.noreply.github.com>
2021-07-09 10:48:11 +0100

    update version of getting data online

--- javaScript Object/javaScriptObject.js
+++ javaScript Object/javaScriptObject.js
@@ -12,12 +12,31 @@
 //    const userTwo = new users('ovovwe', 22, 'ovovwe@gmail.com');
 //    console.log('userOne');
 
+ let uii = document.getElementById('users')
+
 fetch ('https://randomuser.me/api/?results=19')
     .then((resp)=>{
-        console.log(resp)
-        return resp.json()
+       
+        return resp.json()//converting the link output to a json format which sometimes is an array
     }).then((data)=>{
-        console.log(data);
+       
+        let users= data.results;
+        return users.map((user)=>{//the map is use to convert an array to an object
+
+            const li = document.createElement('li'); //we use the create element to an html element and its on a tag element that can be created with the createElement
+            const img = document.createElement('img');
+            const p = document.createElement('p');
+
+            uii.appendChild(li); //the appendChild is to attach an elment to a parent element or another element
+            let paragraph = li.appendChild(p)
+            let image = li.appendChild(img)
+            let firstName = user.name.first;
+            let lastName = user.name.last;
+
+            console.log(user.picture.large)
+            paragraph.innerHTML =`${firstName} ${lastName}`;
+            image.src = user.picture.large; //to get an image we use the scr  and add the link of the image. in this case its an object that is why there is no link
+        })
     }).catch((error)=>{
-        console.log(error);
+       
     })
